9c1517f413dcc03ad26b290efedbb7dc3ef71f2a
Grecia Fuentes <greciafuentes307@gmail.com>
2023-07-20 11:57:26 -0600

    creacion del btn de regreso
    se agrego el boton para regresar a la pagina principal

--- src/pages/CHISTES.js
+++ src/pages/CHISTES.js
@@ -86,6 +86,7 @@ function CHISTES() {
 
     return (
         <div className="principal_chistes">
+            <a href="/principal"><button className="return"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.854 5.646a.5.5 0 010 .708L3.207 9l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M13.5 2.5a.5.5 0 01.5.5v4a2.5 2.5 0 01-2.5 2.5H3a.5.5 0 010-1h8.5A1.5 1.5 0 0013 7V3a.5.5 0 01.5-.5z" clip-rule="evenodd"></path></svg></button></a>
             <h1 className="h1_chistes">Chistes</h1>
             <Tarjetas_Chistes/>
             <button className="btn_generar_chiste" onClick={generar_numero_random}>New chiste</button>
